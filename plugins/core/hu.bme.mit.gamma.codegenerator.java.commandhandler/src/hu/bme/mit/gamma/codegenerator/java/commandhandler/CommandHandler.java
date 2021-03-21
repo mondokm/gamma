b23cd068931185ffa97ddfc320dc4c5ddab661ac
Levente Bajczi <bajczilevi@gmail.com>
2021-03-21 15:31:22 +0000

    Extracted code generation to static method

--- plugins/core/hu.bme.mit.gamma.codegenerator.java.commandhandler/src/hu/bme/mit/gamma/codegenerator/java/commandhandler/CommandHandler.java
+++ plugins/core/hu.bme.mit.gamma.codegenerator.java.commandhandler/src/hu/bme/mit/gamma/codegenerator/java/commandhandler/CommandHandler.java
@@ -23,6 +23,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.URI;
@@ -45,7 +46,7 @@ public class CommandHandler extends AbstractHandler {
 
 	protected Logger logger = Logger.getLogger("GammaLogger");
 	
-	protected final String folderName = "src-gen";
+	protected final static String folderName = "src-gen";
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -56,37 +57,14 @@ public class CommandHandler extends AbstractHandler {
 				if (selection.size() == 1) {
 					if (selection.getFirstElement() instanceof IFile) {
 						IFile file = (IFile) selection.getFirstElement();
+						IProject project = file.getProject();
 						ResourceSet resSet = new ResourceSetImpl();
 						logger.log(Level.INFO, "Resource set for Java code generation created: " + resSet);
 						URI compositeSystemURI = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
 						// Loading the composite system to the resource set
 						Resource resource = loadResource(resSet, compositeSystemURI);
 						Package compositeSystem = (Package) resource.getContents().get(0);
-						// Checking whether all the statecharts have unique names
-						checkStatechartNameUniqueness(file.getProject(), new HashSet<String>());
-						// Getting the simple statechart names
-						Collection<String> simpleStatechartFileNames = getSimpleStatechartFileNames(compositeSystem);
-						// Very important step: recursively attaining the Yakindu-Gamma traces from the project folders based on the imported Statecharts of the composite system
-						List<URI> uriList = new ArrayList<URI>();
-						obtainTraceURIs(file.getProject(), simpleStatechartFileNames, uriList);
-						if (simpleStatechartFileNames.size() != uriList.size()) {
-							logger.log(Level.INFO, "Some trace model is not found: " +
-									simpleStatechartFileNames + System.lineSeparator() + uriList + System.lineSeparator() +
-									"Wrapper is not generated for the Gamma statecharts without trace.");
-						}
-						for (URI uri : uriList) {
-							loadResource(resSet, uri);
-						}
-						// Setting the URI so the composite system code will be generated into a separate package
-						String parentFolder = file.getProject().getLocation() + "/" + folderName;
-						// Decoding so spaces do not stir trouble
-						parentFolder = URI.decode(parentFolder);
-						logger.log(Level.INFO, "Resource set content for Java code generation: " + resSet);
-						String packageName = file.getProject().getName().toLowerCase();
-						GlueCodeGenerator generator = new GlueCodeGenerator(resSet, packageName, parentFolder);
-						generator.execute();
-						generator.dispose();
-						logger.log(Level.INFO, "The Java code generation has been finished.");
+						generateCode(logger, project, resSet, compositeSystem);
 						return null;
 					}
 				}
@@ -98,11 +76,39 @@ public class CommandHandler extends AbstractHandler {
 		}
 		return null;
 	}
+
+	public static void generateCode(Logger logger, IProject project, ResourceSet resSet, Package compositeSystem) throws CoreException {
+		// Checking whether all the statecharts have unique names
+		checkStatechartNameUniqueness( project, new HashSet<String>());
+		// Getting the simple statechart names
+		Collection<String> simpleStatechartFileNames = getSimpleStatechartFileNames(compositeSystem);
+		// Very important step: recursively attaining the Yakindu-Gamma traces from the project folders based on the imported Statecharts of the composite system
+		List<URI> uriList = new ArrayList<URI>();
+		obtainTraceURIs( project, simpleStatechartFileNames, uriList);
+		if (simpleStatechartFileNames.size() != uriList.size()) {
+			logger.log(Level.INFO, "Some trace model is not found: " +
+					simpleStatechartFileNames + System.lineSeparator() + uriList + System.lineSeparator() +
+					"Wrapper is not generated for the Gamma statecharts without trace.");
+		}
+		for (URI uri : uriList) {
+			loadResource(resSet, uri);
+		}
+		// Setting the URI so the composite system code will be generated into a separate package
+		String parentFolder =  project.getLocation() + "/" + folderName;
+		// Decoding so spaces do not stir trouble
+		parentFolder = URI.decode(parentFolder);
+		logger.log(Level.INFO, "Resource set content for Java code generation: " + resSet);
+		String packageName = project.getName().toLowerCase();
+		GlueCodeGenerator generator = new GlueCodeGenerator(resSet, packageName, parentFolder);
+		generator.execute();
+		generator.dispose();
+		logger.log(Level.INFO, "The Java code generation has been finished.");
+	}
 	
 	/**
 	 * Checks whether all statecharts have unique names.
 	 */
-	protected void checkStatechartNameUniqueness(IContainer container, Set<String> fileNames) throws CoreException {
+	protected static void checkStatechartNameUniqueness(IContainer container, Set<String> fileNames) throws CoreException {
 		for (IResource iResource : container.members()) {
 			if (iResource instanceof IFile) {
 				IFile file = (IFile) iResource;
@@ -125,7 +131,7 @@ public class CommandHandler extends AbstractHandler {
 	/**
 	 * Returns the names of the imported statecharts recursively starting from the given project.
 	 */
-	protected Collection<String> getSimpleStatechartFileNames(Package gammaPackage) {
+	protected static Collection<String> getSimpleStatechartFileNames(Package gammaPackage) {
 		Set<String> simpleStatechartNameList = new HashSet<String>();
 		for (Package importedPackage : gammaPackage.getImports()) {
 			if (hasOnlyStatecharts(importedPackage)) {
@@ -142,13 +148,13 @@ public class CommandHandler extends AbstractHandler {
 		return simpleStatechartNameList;
 	}
 	
-	protected boolean hasOnlyStatecharts(Package gammaPackage) {
+	protected static boolean hasOnlyStatecharts(Package gammaPackage) {
 		// Only statecharts (theoretically single statechart) are contained
 		Collection<Component> components = gammaPackage.getComponents();
 		return !components.isEmpty() && components.stream().allMatch(it -> it instanceof StatechartDefinition);
 	}
 	
-	protected String getPackageFileName(Package _package) {
+	protected static String getPackageFileName(Package _package) {
 		URI uri = _package.eResource().getURI();
 		// /hu.bme.mit.gamma.tutorial.extra/model/Monitor/Monitor.gcd
 		String packageFileName = uri.lastSegment();
@@ -160,7 +166,7 @@ public class CommandHandler extends AbstractHandler {
 	/**
 	 * Puts the URIs of the Yakindu-Gamma trace files into the URIList if the trace file has a name contained in importList.
 	 */
-	protected void obtainTraceURIs(IContainer container, Collection<String> importList, List<URI> URIList) throws CoreException {
+	protected static void obtainTraceURIs(IContainer container, Collection<String> importList, List<URI> URIList) throws CoreException {
 		for (IResource iResource : container.members()) {
 			if (iResource instanceof IFile) {
 				IFile file = (IFile) iResource;
@@ -176,7 +182,7 @@ public class CommandHandler extends AbstractHandler {
 		}
 	}
 
-	protected Resource loadResource(ResourceSet resSet, URI uri) throws IllegalArgumentException {
+	protected static Resource loadResource(ResourceSet resSet, URI uri) throws IllegalArgumentException {
 		Resource resource = resSet.getResource(uri, true);
 		EObject object = resource.getContents().get(0);
 		if (!(object instanceof Package || object instanceof Y2GTrace)) {
